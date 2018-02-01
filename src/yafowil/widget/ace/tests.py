bbdef6775b3dd41102f0507db2c937d54174279f
Robert Niederreiter <office@squarewave.at>
2018-02-01 22:37:22 +0100

    Use yafowil.tests.YafowilTestCase

--- src/yafowil/widget/ace/tests.py
+++ src/yafowil/widget/ace/tests.py
@@ -1,14 +1,18 @@
-from node.tests import NodeTestCase
 from node.utils import UNSET
 from yafowil.base import ExtractionError
 from yafowil.base import factory
+from yafowil.tests import YafowilTestCase
 from yafowil.tests import fxml
 import unittest
-import yafowil.widget.ace
 import yafowil.loader
 
 
-class TestACEWidget(NodeTestCase):
+class TestACEWidget(YafowilTestCase):
+
+    def setUp(self):
+        super(TestACEWidget, self).setUp()
+        from yafowil.widget.ace import widget
+        reload(widget)
 
     def test_edit_renderer(self):
         widget = factory('ace', 'acefield', props={'required': True})
