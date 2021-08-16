a193c0e6de7822745b332b6b4844ce620463bbea
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2021-08-16 10:30:15 +0200

    add js files

---
+++ src/yafowil/widget/ace/yafowil.widget.ace.js
@@ -0,0 +1,15 @@
+(function () {
+	'use strict';
+
+
+
+
+	if (window.yafowil === undefined) {
+	    window.yafowil = {};
+	}
+
+	window.yafowil.ace = exports;
+
+
+}());
+//# sourceMappingURL=yafowil.widget.ace.js.map
