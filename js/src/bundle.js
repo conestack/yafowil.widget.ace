6f5a5ba2f16d7d66cc61b40d88b96721f7e13e09
Robert Niederreiter <office@squarewave.at>
2021-10-01 12:41:02 +0200

    Create JS ace widget as class

--- js/src/bundle.js
+++ js/src/bundle.js
@@ -1,1 +1,13 @@
 import $ from 'jquery';
+
+import {AceWidget} from './widget.js';
+
+export * from './widget.js';
+
+$(function() {
+    if (window.ts !== undefined) {
+        ts.ajax.register(AceWidget.initialize, true);
+    } else {
+        AceWidget.initialize();
+    }
+});
--- rollup.conf.js
+++ rollup.conf.js
@@ -1,11 +1,12 @@
 import cleanup from 'rollup-plugin-cleanup';
 import {terser} from 'rollup-plugin-terser';
 
+const out_dir = 'src/yafowil/widget/ace/resources';
+
 const outro = `
 if (window.yafowil === undefined) {
     window.yafowil = {};
 }
-
 window.yafowil.ace = exports;
 `;
 
@@ -16,30 +17,33 @@ export default args => {
             cleanup()
         ],
         output: [{
-            file: 'src/yafowil/widget/ace/yafowil.widget.ace.js',
+            file: `${out_dir}/yafowil.widget.ace.js`,
             format: 'iife',
             outro: outro,
             globals: {
-                jquery: 'jQuery'
+                jquery: 'jQuery',
+                ace: 'ace'
             },
             interop: 'default',
             sourcemap: true,
             sourcemapExcludeSources: true
         }],
         external: [
-            'jquery'
+            'jquery',
+            'ace'
         ]
     };
     if (args.configDebug !== true) {
         conf.output.push({
-            file: 'src/yafowil/widget/ace/yafowil.widget.ace.min.js',
+            file: `${out_dir}/yafowil.widget.ace.min.js`,
             format: 'iife',
             plugins: [
                 terser()
             ],
             outro: outro,
             globals: {
-                jquery: 'jQuery'
+                jquery: 'jQuery',
+                ace: 'ace'
             },
             interop: 'default',
             sourcemap: true,
