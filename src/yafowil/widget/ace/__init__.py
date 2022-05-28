9cc7303fdbf33839ca8938da22d881c270dac5df
Robert Niederreiter <rnix@squarewave.at>
2022-05-28 15:41:03 +0200

    "Adopt webresource registrations"

--- src/yafowil/widget/ace/__init__.py
+++ src/yafowil/widget/ace/__init__.py
@@ -13,31 +13,27 @@ resources_dir = os.path.join(os.path.dirname(__file__), 'resources')
 
 # webresource ################################################################
 
-scripts = wr.ResourceGroup(
-    name='yafowil-ace-scripts',
-    path='yafowil.widget.ace'
+resources = wr.ResourceGroup(
+    name='yafowil-ace-resources',
+    directory=resources_dir,
+    path='yafowil-ace'
 )
-scripts.add(wr.ScriptResource(
+resources.add(wr.ScriptResource(
     name='ace-js',
     directory=os.path.join(resources_dir, 'ace'),
+    path='yafowil-ace/ace',
     resource='ace.js'
 ))
-scripts.add(wr.ScriptResource(
+resources.add(wr.ScriptResource(
     name='yafowil-ace-js',
     depends=[
         'jquery-js',
         'ace-js'
     ],
-    directory=resources_dir,
     resource='widget.js',
     compressed='widget.min.js'
 ))
-
-styles = wr.ResourceGroup(
-    name='yafowil-ace-styles',
-    path='yafowil.widget.ace'
-)
-styles.add(wr.StyleResource(
+resources.add(wr.StyleResource(
     name='yafowil-ace-css',
     directory=resources_dir,
     resource='widget.css'
@@ -69,10 +65,14 @@ css = [{
 def register():
     from yafowil.widget.ace import widget  # noqa
 
+    widget_name = 'yafowil.widget.ace'
+
     # Default
     factory.register_theme(
-        'default', 'yafowil.widget.ace', resources_dir,
-        js=js, css=css
+        'default',
+        widget_name,
+        resources_dir,
+        js=js,
+        css=css
     )
-    factory.register_scripts('default', 'yafowil.widget.ace', scripts)
-    factory.register_styles('default', 'yafowil.widget.ace', styles)
+    factory.register_resources('default', widget_name, resources)
